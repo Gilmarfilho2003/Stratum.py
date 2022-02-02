5e55b7ac93631cbfe38a641e6a5f275c2d85d963
Gilmarfilho2003 <89646761+Gilmarfilho2003@users.noreply.github.com>
2022-02-02 11:40:57 -0300

    Create menu.py

---
+++ menu.py
@@ -0,0 +1,30 @@
+import time
+
+print("Por Favor Aguarde1 estante")
+
+time.sleep(3)
+print('''
+                                                       
+                                                                                                                           
+                                                                                                                                                                                    
+                                                              
+███████╗████████╗██████╗  █████╗ ████████╗██╗   ██╗███╗   ███╗
+██╔════╝╚══██╔══╝██╔══██╗██╔══██╗╚══██╔══╝██║   ██║████╗ ████║
+███████╗   ██║   ██████╔╝███████║   ██║   ██║   ██║██╔████╔██║
+╚════██║   ██║   ██╔══██╗██╔══██║   ██║   ██║   ██║██║╚██╔╝██║
+███████║   ██║   ██║  ██║██║  ██║   ██║   ╚██████╔╝██║ ╚═╝ ██║
+╚══════╝   ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═╝   ╚═╝    ╚═════╝ ╚═╝     ╚═╝
+                                                              
+''')
+
+do = input('''
+               1- Zpool
+               2-zergpool
+               3-Sair
+               
+print('Aguarde')
+
+if do == '1':
+ 
+
+ python pyminer.py -o  stratum+tcp://m7m.sea.mine.zpool.ca:6033 -u dgb1q5amjl97wta2avarpntlkc4kw6x8djkwut0ds2j -p c=DGB
